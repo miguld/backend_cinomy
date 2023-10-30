6ec560bce3b78a5d88d3ead86c229ec09fc51fd2
Juan Esteban Garcés Medina <103276104+jegmdev@users.noreply.github.com>
2023-10-30 17:10:37 -0500

    first commit

---
+++ routes/index.js
@@ -0,0 +1,18 @@
+var router = require('express').Router();
+const { requiresAuth } = require('express-openid-connect');
+
+router.get('/', function (req, res, next) {
+  res.render('index', {
+    title: 'Cinomy',
+    isAuthenticated: req.oidc.isAuthenticated()
+  });
+});
+
+router.get('/profile', requiresAuth(), function (req, res, next) {
+  res.render('profile', {
+    userProfile: JSON.stringify(req.oidc.user, null, 2),
+    title: 'Página Perfil'
+  });
+});
+
+module.exports = router;
